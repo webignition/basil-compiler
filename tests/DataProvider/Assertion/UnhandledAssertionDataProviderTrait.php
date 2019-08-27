f7306f13a49749efef53bbc58fa907ff1825842b
Jon Cram <webignition@gmail.com>
2019-08-27 10:21:29 +0100

    Create empty assertion transpiler (#49)

---
+++ tests/DataProvider/Assertion/UnhandledAssertionDataProviderTrait.php
@@ -0,0 +1,29 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+namespace webignition\BasilTranspiler\Tests\DataProvider\Assertion;
+
+use webignition\BasilModelFactory\AssertionFactory;
+
+trait UnhandledAssertionDataProviderTrait
+{
+    public function unhandledAssertionDataProvider(): array
+    {
+        $assertionFactory = AssertionFactory::createFactory();
+
+        return [
+            'is comparison, element identifier examined value, literal string expected value' => [
+                'assertion' => $assertionFactory->createFromAssertionString(
+                    '".selector" is "value"'
+                ),
+            ],
+            'is comparison, attribute identifier examined value, literal string expected value' => [
+                'assertion' => $assertionFactory->createFromAssertionString(
+                    '".selector".attribute_name is "value"'
+                ),
+            ],
+        ];
+    }
+}
