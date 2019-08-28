d7e30187216afc17c6f6520552826f2b7e9ea7a9
Jon Cram <webignition@gmail.com>
2019-08-28 10:27:42 +0100

    Resolve master changes (#79)

--- src/Assertion/AssertionTranspiler.php
+++ src/Assertion/AssertionTranspiler.php
@@ -2,23 +2,20 @@
 
 namespace webignition\BasilTranspiler\Assertion;
 
-use webignition\BasilTranspiler\AbstractDelegatedTranspiler;
+use webignition\BasilTranspiler\AbstractDelegatingTranspiler;
 use webignition\BasilTranspiler\TranspilerInterface;
-use webignition\BasilTranspiler\VariableNameResolver;
 
-class AssertionTranspiler extends AbstractDelegatedTranspiler implements TranspilerInterface
+class AssertionTranspiler extends AbstractDelegatingTranspiler implements TranspilerInterface
 {
     public static function createTranspiler(): AssertionTranspiler
     {
-        return new AssertionTranspiler(
-            new VariableNameResolver()
-        );
+        return new AssertionTranspiler();
     }
 
     public function handles(object $model): bool
     {
         if ($model instanceof AssertionTranspiler) {
-            return null !== $this->findIdentifierTypeTranspiler($model);
+            return null !== $this->findDelegatedTranspiler($model);
         }
 
         return false;
