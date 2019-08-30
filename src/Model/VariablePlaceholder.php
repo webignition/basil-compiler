3e9b940ab7621bf95ffc1698954cba4242b6ac93
Jon Cram <webignition@gmail.com>
2019-08-30 08:35:48 +0100

    Optionally provide a VariablePlaceholder id (#104)

--- src/Model/VariablePlaceholder.php
+++ src/Model/VariablePlaceholder.php
@@ -7,15 +7,19 @@ class VariablePlaceholder implements UniqueItemInterface
     const TEMPLATE = '{{ %s }}';
 
     private $name = '';
+    private $id = '';
 
-    public function __construct(string $name)
+    public function __construct(string $name, string $id = '')
     {
+        $id = '' === $id ? $name : $id;
+
         $this->name = $name;
+        $this->id = $id;
     }
 
     public function getId(): string
     {
-        return $this->name;
+        return $this->id;
     }
 
     public function __toString(): string
