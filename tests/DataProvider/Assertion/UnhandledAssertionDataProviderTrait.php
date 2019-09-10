60b7b8bfd28fdf082cd43067c78cfa6df23707a5
Jon Cram <jon@webignition.net>
2019-09-10 12:06:36 +0100

    Update assertion transpilers

--- tests/DataProvider/Assertion/UnhandledAssertionDataProviderTrait.php
+++ tests/DataProvider/Assertion/UnhandledAssertionDataProviderTrait.php
@@ -5,14 +5,10 @@ declare(strict_types=1);
 
 namespace webignition\BasilTranspiler\Tests\DataProvider\Assertion;
 
-use webignition\BasilModelFactory\AssertionFactory;
-
 trait UnhandledAssertionDataProviderTrait
 {
     public function unhandledAssertionDataProvider(): array
     {
-        $assertionFactory = AssertionFactory::createFactory();
-
         return [
             'unhandled assertion: non-value object' => [
                 'value' => new \stdClass(),
