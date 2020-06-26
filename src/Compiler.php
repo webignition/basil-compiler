afdb7fd55ce73e379c95bfffbf4bcdeb93f1a855
Jon Cram <webignition@gmail.com>
2020-06-26 09:56:57 +0100

    Compiler::compile() should take ClassDefinitionInterface as the first arg

--- src/Compiler.php
+++ src/Compiler.php
@@ -2,12 +2,8 @@
 
 namespace webignition\BasilCompiler;
 
-use webignition\BasilCompilableSource\ClassDefinition;
 use webignition\BasilCompilableSource\ClassDefinitionInterface;
-use webignition\BasilCompilableSource\Expression\ClassDependency;
 use webignition\BasilCompilableSourceFactory\ClassDefinitionFactory;
-use webignition\BasilCompilableSourceFactory\Exception\UnsupportedStepException;
-use webignition\BasilModels\Test\TestInterface;
 
 class Compiler
 {
@@ -35,22 +31,14 @@ class Compiler
     }
 
     /**
-     * @param TestInterface $test
-     * @param string $fullyQualifiedBaseClass
+     * @param ClassDefinitionInterface $classDefinition
      *
      * @return string
      *
      * @throws UnresolvedPlaceholderException
-     * @throws UnsupportedStepException
      */
-    public function compile(TestInterface $test, string $fullyQualifiedBaseClass): string
+    public function compile(ClassDefinitionInterface $classDefinition): string
     {
-        $classDefinition = $this->createClassDefinition($test);
-
-        if ($classDefinition instanceof ClassDefinition) {
-            $classDefinition->setBaseClass(new ClassDependency($fullyQualifiedBaseClass));
-        }
-
         $compiledClass = $classDefinition->render();
         $compiledClassLines = explode("\n", $compiledClass);
 
@@ -64,29 +52,5 @@ class Compiler
         }
 
         return implode("\n", $resolvedLines);
-    }
-
-    /**
-     * @param TestInterface $test
-     *
-     * @return string
-     *
-     * @throws UnsupportedStepException
-     */
-    public function createClassName(TestInterface $test): string
-    {
-        return $this->createClassDefinition($test)->getName();
-    }
-
-    /**
-     * @param TestInterface $test
-     *
-     * @return ClassDefinitionInterface
-     *
-     * @throws UnsupportedStepException
-     */
-    private function createClassDefinition(TestInterface $test): ClassDefinitionInterface
-    {
-        return $this->classDefinitionFactory->createClassDefinition($test);
     }
 }
